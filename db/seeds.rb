0a0b62ae7b2f863463ae80b8e317cf4fee3514f9
ntwigamartin <ntwiga.martin@gmail.com>
2023-03-28 16:02:26 +0300

    ruby v update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Location.create(county: "Nairobi", street: "Moi Avenue")
+# Location.create(county: "Nairobi", street: "Ngong Road")
+# Location.create(county: "Nairobi", street: "Tom Mboya")
+# Location.create(county: "Mombasa", street: "Digo Road")
+# Location.create(county: "Mombasa", street: "Moi Avenue")
+# Location.create(county: "Mombasa", street: "Beach Front")
