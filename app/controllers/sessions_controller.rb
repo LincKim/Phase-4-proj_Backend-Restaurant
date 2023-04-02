3e7c75a65706094f5175ed1e79909b10dbf2d9f4
LincKim <lincolnkimutai@gmail.com>
2023-04-02 22:12:13 +0300

    Login catch errors added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,12 @@
 class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:username])
+        if user
         session[:user_id] = user.id
         render json: user
+        else
+            render json: { message: "user not found" }, status: :not_found
+        end
     end
 
     def destroy
